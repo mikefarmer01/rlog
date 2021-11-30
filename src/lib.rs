85ced8cf0bc4b9919044715c2b13749fb2c03a66
Michael Bauer <mikefarmer01@web.de>
2021-11-30 10:20:41 +0100

    DemandManagement as pure struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
-mod utils;
 mod log;
+mod utils;
 
 use wasm_bindgen::prelude::*;
 
-use crate::log::{DemandManagement, run};
+use crate::log::DemandManagement;
 use statrs::distribution::Normal;
-
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -14,7 +13,7 @@ use statrs::distribution::Normal;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
@@ -24,10 +23,21 @@ pub fn greet() {
 }
 
 #[wasm_bindgen]
-pub fn demand() -> JsValue{
+pub fn demand() -> JsValue {
     let normal_distr = Normal::new(30.0, 2.0).unwrap();
     let alpha = 0.1;
-    let dm: DemandManagement = run(&normal_distr, alpha);
+    let mut dm: DemandManagement<Normal> = DemandManagement {
+        demands: Vec::<f32>::new(),
+        demands_estimated: Vec::<f32>::new(),
+        distr: normal_distr,
+        alpha: alpha,
+        rng: rand::prelude::thread_rng()
+    };
+    //dm.init(normal_distr, alpha);
+    
+    let n = 50;
+    dm.period_zero();
+    dm.run_periods(n);
     let periods = dm.to_periods();
 
     JsValue::from_serde(&periods).unwrap()
--- src/log.rs
+++ src/log.rs
@@ -1,69 +1,77 @@
-use rand::prelude::{ThreadRng, thread_rng};
 use rand::distributions::Distribution;
+use rand::prelude::{thread_rng, ThreadRng};
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-pub fn run(distr: &impl Distribution<f64>, alpha: f32) -> DemandManagement{
-    let mut dm: DemandManagement = init(distr);
-    for _i in 0..100{
-        dm = period(dm, distr, alpha);
-    }
-    return dm;
-}
-
-fn period(mut dm: DemandManagement, distr: &impl Distribution<f64>, alpha: f32) -> DemandManagement{
-    make_demand(&mut dm.demands, distr);
-    estimate_demand(&mut dm, alpha);
-    return dm;
-}
-
-fn make_demand(demands: &mut Vec<f32>, distr: &impl Distribution<f64>){
-    let mut rng: ThreadRng = thread_rng();
-    let demand: f32 = distr.sample(&mut rng).round() as f32;
-    demands.push(demand);
-}
-
-fn estimate_demand(dm: &mut DemandManagement, alpha: f32){
-    let demand_smoothed = smooth_exponentially(dm, alpha);
-    dm.demands_estimated.push(demand_smoothed);
-}
-
-fn smooth_exponentially(dm: &mut DemandManagement, alpha: f32) -> f32{
-    let demand: f32 = dm.demands.last().cloned().unwrap();
-    let last_demand_estimated: f32 = dm.demands_estimated.last().cloned().unwrap();
-    let demand_estimated: f32  = alpha * demand + (1.0-alpha) * last_demand_estimated;
-    return demand_estimated;
-}
-
-fn init(distr: &impl Distribution<f64>) -> DemandManagement{
-    let mut dm: DemandManagement = DemandManagement {
-        demands: Vec::<f32>::new(),
-        demands_estimated: Vec::<f32>::new()
-    };
-    make_demand(&mut dm.demands, distr);
-    dm.demands_estimated.push(dm.demands.last().cloned().unwrap());
-    return dm;
-}
-
-pub struct DemandManagement {
+pub struct DemandManagement<T: Distribution<f64>> {
     pub demands: Vec<f32>,
     pub demands_estimated: Vec<f32>,
-    
+    pub distr: T,
+    pub alpha: f32,
+    pub rng: ThreadRng,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Period{
+pub struct Period {
     ind: usize,
     demand: f32,
-    demand_estimated: f32
+    demand_estimated: f32,
 }
 
-impl DemandManagement {
+impl<T: Distribution<f64>> DemandManagement<T> {
+    pub fn init(&mut self, distr: T, alpha: f32) {
+        self.demands = Vec::<f32>::new();
+        self.demands_estimated = Vec::<f32>::new();
+        self.distr = distr;
+        self.alpha = alpha;
+        self.rng = thread_rng();
+        self.period_zero();
+    }
+    pub fn period_zero(&mut self) {
+        self.make_demand();
+        self.demands_estimated
+            .push(self.demands.last().cloned().unwrap());
+    }
+    pub fn run_periods(&mut self, n: i32) {
+        for _i in 0..n {
+            self.run_period();
+        }
+    }
+    fn run_period(&mut self) {
+        self.make_demand();
+        self.estimate_demand();
+    }
+    fn make_demand(&mut self) {
+        let demand: f32 = self.distr.sample(&mut self.rng).round() as f32;
+        self.demands.push(demand);
+    }
+    fn estimate_demand(&mut self) {
+        let demand_smoothed = self.smooth_exponentially();
+        self.demands_estimated.push(demand_smoothed);
+    }
+
+    fn smooth_exponentially(&self) -> f32 {
+        let demand: f32 = self.demands.last().cloned().unwrap();
+        let last_demand_estimated: f32 = self.demands_estimated.last().cloned().unwrap();
+        let demand_estimated: f32 =
+            self.alpha * demand + (1.0 - self.alpha) * last_demand_estimated;
+        return demand_estimated;
+    }
+
     pub fn to_periods(&self) -> Vec<Period> {
         let d = &self.demands;
         let e = &self.demands_estimated;
-        
-        let periods: Vec<Period> = d.into_iter().enumerate().zip(e.into_iter()).map(|((a,b), c)| Period { ind: a, demand: *b, demand_estimated: *c}).collect();
+
+        let periods: Vec<Period> = d
+            .into_iter()
+            .enumerate()
+            .zip(e.into_iter())
+            .map(|((a, b), c)| Period {
+                ind: a,
+                demand: *b,
+                demand_estimated: *c,
+            })
+            .collect();
         return periods;
     }
 }
